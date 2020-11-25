0cc390fe23869fe454c8f88a399521c6fa5fa5da
brayo303 <47560915+brayo303@users.noreply.github.com>
2020-11-25 19:40:04 +0700

    last hari ini

--- app/src/main/java/com/example/p3bpianotiles/MainMenuFragment.java
+++ app/src/main/java/com/example/p3bpianotiles/MainMenuFragment.java
@@ -7,13 +7,18 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+
+import com.example.p3bpianotiles.databinding.MainMenuFragmentBinding;
+
 public class MainMenuFragment extends Fragment {
     //binding here
+    private MainMenuFragmentBinding binding;
     public MainMenuFragment(){
 
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container , Bundle savedInstance){
-        return null;
+        binding = MainMenuFragmentBinding.inflate(inflater);
+        return binding.getRoot();
     }
 }
