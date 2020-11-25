52dbd093ed630eebba8ec711a1c2bed5bb1cbecc
DeddyChandra <6181801007@student.unpar.ac.id>
2020-11-25 17:42:48 +0700

    binding

--- app/src/main/java/com/example/p3bpianotiles/MainActivity.java
+++ app/src/main/java/com/example/p3bpianotiles/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(this.binding.getRoot());
     }
 }
