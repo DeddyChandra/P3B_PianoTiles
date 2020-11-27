91d015e178eb8acb5f89efec1c9afbc3b75aa6ed
Michael <6181801041@student.unpar.ac.id>
2020-11-27 20:20:19 +0700

    Merge branch 'master' of D:\SEMESTER 5\P3B\P3B_PianoTiles with conflicts.

--- app/src/main/java/com/example/p3bpianotiles/MainActivity.java
+++ app/src/main/java/com/example/p3bpianotiles/MainActivity.java
@@ -2,13 +2,12 @@ package com.example.p3bpianotiles;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.example.p3bpianotiles.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity implements FragmentListener {
+public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private FragmentManager fragmentManager;
     MainMenuFragment mainMenuFragment;
@@ -18,21 +17,15 @@ public class MainActivity extends AppCompatActivity implements FragmentListener 
         super.onCreate(savedInstanceState);
         this.binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(this.binding.getRoot());
-        mainMenuFragment = new MainMenuFragment();
-        gameplayFragment = new GameplayFragment();
-        fragmentManager = this.getSupportFragmentManager();
-
+        mainMenuFragment=new MainMenuFragment();
+        gameplayFragment=new GameplayFragment();
     }
 
+    void changePage(int page){
+        if(page==0){
 
-    @Override
-    public void changePage(int page){
-        FragmentTransaction ft = this.fragmentManager.beginTransaction();
-        if(page == 1){
-            ft.replace(R.id.fragment_container, this.mainMenuFragment).addToBackStack(null);
-        } else if(page == 2){
-            ft.replace(R.id.fragment_container, this.gameplayFragment).addToBackStack(null);
         }
-        ft.commit();
+
+
     }
 }
