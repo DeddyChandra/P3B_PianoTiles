ba60c6291f7848d94d947175519e1bbee6edab1a
Michael <6181801041@student.unpar.ac.id>
2020-11-27 20:22:24 +0700

    changepage

--- app/src/main/java/com/example/p3bpianotiles/MainActivity.java
+++ app/src/main/java/com/example/p3bpianotiles/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.p3bpianotiles;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.example.p3bpianotiles.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentListener{
     private ActivityMainBinding binding;
     private FragmentManager fragmentManager;
     MainMenuFragment mainMenuFragment;
@@ -19,13 +20,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(this.binding.getRoot());
         mainMenuFragment=new MainMenuFragment();
         gameplayFragment=new GameplayFragment();
+        fragmentManager = this.getSupportFragmentManager();
+
     }
 
-    void changePage(int page){
-        if(page==0){
-
+    public void changePage(int page){
+        FragmentTransaction ft = this.fragmentManager.beginTransaction();
+        if(page == 1){
+            ft.replace(R.id.fragment_container, this.mainMenuFragment).addToBackStack(null);
+        } else if(page == 2){
+            ft.replace(R.id.fragment_container, this.gameplayFragment).addToBackStack(null);
         }
-
+        ft.commit();
 
     }
 }
