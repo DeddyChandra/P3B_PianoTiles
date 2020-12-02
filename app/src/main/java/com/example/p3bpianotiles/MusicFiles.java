966d8c41a2b9e1a66df87cf844c84464f563d597
DeddyChandra <6181801007@student.unpar.ac.id>
2020-12-02 11:07:23 +0700

    song

--- app/src/main/java/com/example/p3bpianotiles/MusicFiles.java
+++ app/src/main/java/com/example/p3bpianotiles/MusicFiles.java
@@ -2,8 +2,9 @@ package com.example.p3bpianotiles;
 
 public class MusicFiles {
     public static Music[] music ={
-            new Music (R.raw.jingle_bells, "Jinggle Bell Piano"),
-            new Music (R.raw.silent_night, "Silent Night Piano"),
-            new Music (R.raw.last_christmas, "Last Christmas Piano")
+            new Music (R.raw.jingle_bells, "Jingle Bell"),
+            new Music (R.raw.silent_night, "Silent Night"),
+            new Music (R.raw.last_christmas, "Last Christmas"),
+            new Music (R.raw.xia_yu_tian, "下雨天")
     };
 }
