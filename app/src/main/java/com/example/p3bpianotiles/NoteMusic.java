b8c85856186b37a3df4e22d4b0770c3a50c89978
Michael <6181801041@student.unpar.ac.id>
2020-12-03 12:57:03 +0700

    ArrayNote

---
+++ app/src/main/java/com/example/p3bpianotiles/NoteMusic.java
@@ -0,0 +1,15 @@
+package com.example.p3bpianotiles;
+
+public class NoteMusic {
+    int note;
+
+    public int getNote() {
+        return note;
+    }
+    public void setNote(int note) {
+        this.note = note;
+    }
+    public NoteMusic(int note) {
+        this.note = note;
+    }
+}
