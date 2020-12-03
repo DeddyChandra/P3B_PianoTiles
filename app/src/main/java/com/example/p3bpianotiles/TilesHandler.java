13e51da240148b1187bc734ab1799f40824a50b4
brayo303 <47560915+brayo303@users.noreply.github.com>
2020-12-03 20:42:07 +0700

    masuk

--- app/src/main/java/com/example/p3bpianotiles/TilesHandler.java
+++ app/src/main/java/com/example/p3bpianotiles/TilesHandler.java
@@ -22,16 +22,16 @@ public class TilesHandler extends Handler {
             presenter.drawRedrawTiles((Object[])msg.obj);
 
         }
-        else if(msg.what== TilesHandler.MSG_DELTE){
+        if(msg.what== TilesHandler.MSG_DELTE){
             presenter.delete((Object[])msg.obj);
         }
-        else if(msg.what==2){
+        if(msg.what==2){
             Log.d("a","a");
             presenter.generate((Object[])msg.obj);
         }
-        else if(msg.what == TilesHandler.ADD_SCORE) {
+        /*if(msg.what == TilesHandler.ADD_SCORE) {
             this.gameplayFragment.addScore();
-        }
+        }*/
     }
     public void setMessage(Object[] arr, int i){
         Message msg= new Message();
