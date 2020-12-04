39d8e60d522d0b8f267f8f252edc145dbd49c89a
DeddyChandra <6181801007@student.unpar.ac.id>
2020-12-04 22:35:03 +0700

    score

--- app/src/main/java/com/example/p3bpianotiles/GameplayPresenter.java
+++ app/src/main/java/com/example/p3bpianotiles/GameplayPresenter.java
@@ -126,7 +126,7 @@ public class GameplayPresenter implements GameplayContract.Presenter {
             Log.d("delete","mati");
         }else{
             addScore();
-            tilesArrayList.get(idxclicked).isAddedScore();
+            tilesArrayList.get(idxclicked).setAddedScore(false);
             Log.d("delete","delete");
             tilesArrayList.get(idxclicked).setToBeDelete(true);
         }
