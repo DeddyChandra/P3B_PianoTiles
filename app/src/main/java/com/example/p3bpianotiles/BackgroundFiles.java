73809a41ca80b47ba30855eb7c5bb72241da04bd
DeddyChandra <6181801007@student.unpar.ac.id>
2020-12-06 14:12:31 +0700

    background

--- app/src/main/java/com/example/p3bpianotiles/BackgroundFiles.java
+++ app/src/main/java/com/example/p3bpianotiles/BackgroundFiles.java
@@ -5,6 +5,7 @@ public class BackgroundFiles {
             new Background(R.drawable.background, "Snow"),
             new Background(R.drawable.background2, "Piano"),
             new Background(R.drawable.background3, "Nintendo"),
-            new Background(R.drawable.background4, "Ladder")
+            new Background(R.drawable.background4, "Ladder"),
+            new Background(R.drawable.background5, "Christmas")
     };
 }
